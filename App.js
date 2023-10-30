2805e8564caff6f25035639d8b71782a267ab99d
bobby <ande970s@stud.kea.dk>
2023-10-30 14:04:46 +0100

    :)

--- App.js
+++ App.js
@@ -1,16 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { PanGestureHandler, GestureHandlerRootView } from 'react-native-gesture-handler';
+import Animated, {useSharedValue, useAnimatedGestureHandler, useAnimatedStyle, withSpring} from 'react-native-reanimated'
+
+
 
 export default function App() {
+
+  const translateX = useSharedValue(0)
+  const translateY = useSharedValue(0)
+
+  const onGestureEvent = useAnimatedGestureHandler({
+    onStart: (_,context) => {
+      console.log("Start: Der sker noget " + translateX.value)
+
+    }
+  })
+  
+
+
   return (
+   <GestureHandlerRootView style = {styles.rootView} > 
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <PanGestureHandler onGestureEvent={onGestureEvent}>
+        <Animated.View>
+      <Text>Drag and watch console log</Text>
+        </Animated.View>
+      </PanGestureHandler>
       <StatusBar style="auto" />
     </View>
+    </GestureHandlerRootView>
   );
-}
 
+}
 const styles = StyleSheet.create({
+  rootView:{
+    flex: 1
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
@@ -18,3 +44,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
